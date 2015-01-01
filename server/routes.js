1bf97db7e6f199677ef6ce7b3f71f8817dd22210
Liam Schauerman <ljschauerman@gmail.com>
2015-01-01 13:32:34 -0800

    refactor weekly stats

--- server/routes.js
+++ server/routes.js
@@ -84,13 +84,16 @@ exports.myMatchups = function(req, res) {
         .done(function(data) {
 
             var matchups = data.fantasy_content.team[1].matchups;
+            var matchupTotals = data.fantasy_content.team[1].matchups[0].matchup[0].teams[0].team[1].team_stats.stats;
+            var weeklyStats = {};
+            for( var i = 0; i < matchupTotals.length; i++ ) {
+                weeklyStats[ categoryCodes[ matchupTotals[i].stat.stat_id ] ] = matchupTotals[i].stat.value;
+            }
             // matchups --> matchups[0].matchup[0].teams[0].team[1].team_stats.stats is an array of objects
             // get to matchups[0].matchup[0].teams[0].team[1].team_stats.stats
             // this is an array of objects, 1 for each stat
 
             // matchups[0].matchup[0].teams[0].team[1].team_stats.stats[]
-            res.json(data)
-            // matchupTotals = data[0].matchup[0].teams[0].team[1].team_stats.stats
             var categoryCodes = {
                 9004003 : "FG%",
                 9007006 : "FT%",
@@ -102,6 +105,9 @@ exports.myMatchups = function(req, res) {
                 17: "STL",
                 18: "BLK"
             }
+
+
+            res.json(weeklyStats)
             // res.json(matchupTotals);
         });
 };
