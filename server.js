1779f3f1dbc014497ba66f3fc52cc3bd2c38470d
Liam Schauerman <ljschauerman@gmail.com>
2015-01-03 16:50:23 -0800

    async

--- server.js
+++ server.js
@@ -2,14 +2,9 @@ var express = require('express');
 var http = require('http');
 var routes = require('./server/routes.js');
 var url = require('url');
-// var bodyParser  = require('body-parser'),
-// var routes = require('server/routes.js');
 app = express();
 app.engine('html', require('ejs').renderFile);
-// app.set('view engine', 'html');
 app.set('views', __dirname + '/client');
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.use(bodyParser.json());
 routes(app, express);
 
 
