0be9cd3a0c49b576d2714b6eed42e3c37d6b6393
Liam Schauerman <ljschauerman@gmail.com>
2015-01-04 22:42:48 -0800

    contenttype

--- server/routes.js
+++ server/routes.js
@@ -250,7 +250,7 @@ module.exports = function (app, express) {
     app.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", '*');
         res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-        res.header("Access-Control-Allow-Headers",  'Content-Type');
+        res.header("Access-Control-Allow-Headers",  'Content-Type, X-Requested-With');
         next();
     });
 
@@ -265,12 +265,12 @@ module.exports = function (app, express) {
 
     // });
 
-  app.use('/', function (req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "X-Requested-With");
-      res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-      next();
-  });
+  // app.use('/', function (req, res, next) {
+  //     res.header("Access-Control-Allow-Origin", "*");
+  //     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  //     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+  //     next();
+  // });
   app.get('/', function(req, res){
       // console.log(__dirname + '/client/index.html')
       res.render('index.html')
