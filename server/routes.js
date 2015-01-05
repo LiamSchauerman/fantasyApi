d8069ad96552223122da4e8a8ec09d97942db506
Liam Schauerman <ljschauerman@gmail.com>
2015-01-04 22:40:32 -0800

    header

--- server/routes.js
+++ server/routes.js
@@ -265,9 +265,10 @@ module.exports = function (app, express) {
 
     // });
 
-  app.get('/', function (req, res, next) {
+  app.use('/', function (req, res, next) {
       res.header("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
       next();
   });
   app.get('/', function(req, res){
