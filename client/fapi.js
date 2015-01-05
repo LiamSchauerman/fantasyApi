8a60602b74c4d2cda4a34ca78193e36170203275
Liam Schauerman <ljschauerman@gmail.com>
2015-01-04 18:46:47 -0800

    add js

--- client/fapi.js
+++ client/fapi.js
@@ -1,13 +1,76 @@
 var call = function(){
-	$.get({
-	  url:"http://fantasysports.yahooapis.com/fantasy/v2/game/nba", 
-	  success: function(data){
-	  	cosole.log('in success');
-	    console.log(data);
-	  },
-	  error: function(data){
-	  	console.log('in error')
-	  }
-
+	console.log('test');
+	$.get("http://yahoofantasyapi.herokuapp.com/allmatchups", function(data){
+		console.log(data)
 	})
 }
+
+// BAR GRAPH SECTION
+
+var barGraphUpdate = function(data){
+  var bars;
+  // set ratio instead of always *setWidthpx // total width is 820px
+  // largest row should be 800 px
+  // coefficient = 800/mostMade
+  console.log( data )
+  var setWidth = 800 / (data[0].shotsMade + data[0].shotsMissed);
+
+
+  // JOIN
+
+  bars = d3.select("#chart").selectAll(".bar")
+        .data(data)
+
+  // exit
+  console.log(bars.exit())
+  bars.exit()
+    .style('opacity', 1)
+    .transition()
+      .duration(750)
+      .style('opacity', 0)
+    .remove()
+
+  // UPDATE
+  bars
+  .transition().duration(500)
+    .style({
+      width: function(d) { return (d.shotsMade+d.shotsMissed) * setWidth + "px"; }, }) .select('.madeShots') // shots made
+      .attr('class', 'madeShots')
+      .style({
+        width: function(d) { return d.shotsMade*setWidth + 'px' } }) .select('.text') // text about player and shots
+      .attr('class','text')
+      .text(function(d) {
+        return d.name + ': ' + d.shotsMade+'/'+(d.shotsMissed+d.shotsMade);
+      })
+
+//ENTER
+  var containerBars = bars.enter()
+    .append("div") // total shots
+      .style('width', '0px')
+      .attr("class", 'bar')
+
+  // var shotsMadeBars = containerBars.append('div')
+  //     .style('width', '0px')
+  //     .attr('class', 'madeShots')
+
+
+  // var textBars = shotsMadeBars.append('div')
+  //     .style('opacity', 0)
+  //     .attr('class','text')
+  //       .text(function(d) {
+  //         return d.name + ': ' + d.shotsMade+'/'+(d.shotsMissed+d.shotsMade);
+  //       })
+
+    containerBars.transition()
+      .duration(750)
+      .style({ width: function(d) { return (d.shotsMade+d.shotsMissed) * setWidth + "px"; }, })
+    // shotsMadeBars.transition()
+    //   .duration(500)
+    //   .style({ width: function(d) { return d.shotsMade*setWidth + 'px' } })
+    // textBars.transition()
+    //   .duration(500)
+    //   .style('opacity', 1)
+
+        
+
+}; // end barGraphUpdate();
--- server.js
+++ server.js
@@ -7,7 +7,11 @@ app.engine('html', require('ejs').renderFile);
 app.set('views', __dirname + '/client');
 routes(app, express);
 
-
+app.get('/', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
 
 
 var port = process.env.PORT || 8000;
